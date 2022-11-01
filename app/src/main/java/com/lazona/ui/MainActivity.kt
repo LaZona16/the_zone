e166d4e13831368a6c7add8b03077dfdcf9c1e15
Brayan Garcia <brayangarcia44@hotmail.com>
2022-10-31 21:48:11 -0500

    Implements of scan bluetooth

--- app/src/main/java/com/lazona/ui/MainActivity.kt
+++ app/src/main/java/com/lazona/ui/MainActivity.kt
@@ -5,14 +5,17 @@ import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.location.LocationManager
 import android.os.Bundle
 import android.os.ParcelUuid
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -23,8 +26,11 @@ import com.lazona.databinding.ActivityMainBinding
 import com.lazona.ui.connectdevice.ConnectWallAdapter
 
 private const val LOCATION_PERMISSIONS = 2
+private const val WALL_SERVICE_UUID = "0000ffe0-0000-1000-8000-00805f9b34fb"
+private const val WALL_CHARACTERISTIC_UUID = "0000ffe1-0000-1000-8000-00805f9b34fb"
 
 class MainActivity : AppCompatActivity() {
+
 
     private lateinit var bluetoothManager: BluetoothManager
     private val bluetoothAdapter: BluetoothAdapter by lazy { bluetoothManager.adapter }
@@ -49,13 +55,42 @@ class MainActivity : AppCompatActivity() {
                     checkPermissions()
                     val deviceName = device?.name
                     val deviceHardwareAddress = device?.address // MAC address
-                    binding.recyclerViewDevices.adapter = ConnectWallAdapter(deviceList)
                 }
-                else -> Toast.makeText(applicationContext, "Bluetooth EROOR", Toast.LENGTH_SHORT)
+                else -> Toast.makeText(applicationContext, "Bluetooth ERROR", Toast.LENGTH_SHORT)
                     .show()
             }
         }
     }
+
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+    private val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+        .build()
+    private val scanCallback = object : ScanCallback() {
+        @SuppressLint("MissingPermission")
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            Log.d("HOLA2", result.device.name ?: "UNNAMED")
+
+            deviceList.add(result.device.name)
+            binding.recyclerViewDevices.adapter?.notifyDataSetChanged()
+
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            super.onScanFailed(errorCode)
+            Log.d("HOLA2", "ERROR")
+        }
+    }
+    private var isScanning = false
+        set(value) {
+            field = value
+            runOnUiThread {
+                binding.searchBluetoothDevicesButton.text = if (value) "Stop Scan" else "Start Scan"
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,8 +99,16 @@ class MainActivity : AppCompatActivity() {
 
         setUpPermissions()
         // Register for broadcasts when a device is discovered.
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
+        //val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        //registerReceiver(receiver, filter)
+        binding.recyclerViewDevices.adapter = ConnectWallAdapter(deviceList)
+        binding.searchBluetoothDevicesButton.setOnClickListener {
+            if (isScanning) {
+                stopBleScan()
+            } else {
+                startBluetoothScan()
+            }
+        }
     }
 
     private fun setUpPermissions() {
@@ -146,13 +189,24 @@ class MainActivity : AppCompatActivity() {
                     LOCATION_PERMISSIONS
                 )
             }
-            else -> startBluetoothScan()
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun startBluetoothScan() {
         val filter = ScanFilter.Builder().setServiceUuid(
-            ParcelUuid.fromString(ENVIRONMENTAL_SERVICE_UUID.toString())
+            ParcelUuid.fromString(WALL_SERVICE_UUID)
         ).build()
+        isScanning = true
+//        bleScanner.startScan(scanCallback)
+        bleScanner.startScan(null, scanSettings, scanCallback)
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun stopBleScan() {
+        bleScanner.stopScan(scanCallback)
+//        bluetoothAdapter.bluetoothLeScanner.stopScan(scanCallback)
+        isScanning = false
     }
 }
