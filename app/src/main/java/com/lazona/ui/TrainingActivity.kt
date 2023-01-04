5abd5bc678d6408e751c445f71bd6b66fcaad68b
Brayan Garcia <brayangarcia44@hotmail.com>
2023-01-03 23:06:33 -0500

    training timer with no of passes ui

---
+++ app/src/main/java/com/lazona/ui/TrainingActivity.kt
@@ -0,0 +1,13 @@
+package com.lazona.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lazona.R
+
+class TrainingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_training)
+    }
+
+}
